de851aee6cb79b1260d1a9c03121fac35ab0f969
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-03-11 14:27:39 -0800

    Add tasks

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines the city class"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Defines city attributes"""
+    state_id = ""
+    name = ""
---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines review class"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Defines review attributes"""
+    place_id = ""
+    user_id = ""
+    text = ""
