c988690604ec42cc00c71889236623f0228511b0
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-03-13 14:13:20 -0700

    Add tasks

--- models/base_model.py
+++ models/base_model.py
@@ -17,8 +17,8 @@ class BaseModel:
                 if key != "__class__":
                     if key in ["created_at", "updated_at"]:
                         setattr(self, key, datetime.fromisoformat(value))
-                else:
-                    setattr(self, key, value)
+                    else:
+                        setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
